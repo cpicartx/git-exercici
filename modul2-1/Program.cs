45033229bc5939b77342ab6d8e3dbda645859312
cristian <cpicartx@gmail.com>
2020-09-30 20:29:36 +0200

    Modul2-1 fase1

---
+++ modul2-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace modul2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercici variables....
+            // FASE 1
+            string nom = "Juan";
+            string cognom1 = "Asensio";
+            string cognom2 = "Rodríguez";
+            int dia = 18;
+            int mes = 10;
+            int any = 1980;
+
+
+            Console.WriteLine(cognom1 + " " + cognom2 + ", " + nom);
+            Console.WriteLine(dia + "/" + mes + "/" + any);
+
+            // FASE2
+
+        }
+    }
+}
